79891acc48a01d4e4178ff76a47418d0ba2ddd34
saji-ryu <t15443rs@sfc.keio.ac.jp>
2015-10-15 10:28:09 +0900

    ex05,ex06

---
+++ js/ex05.js
@@ -0,0 +1,7 @@
+var i = 0;
+var sum = 0;
+while (i < 5) {
+  sum = sum + 1;
+  i = i + 1;
+}
+//console.log(sum);
---
+++ js/ex06.js
@@ -0,0 +1,7 @@
+var i = 0;
+var sum = 0;
+while (i <= 100) {
+  sum = sum + i;
+  i = i + 1;
+}
+console.log(sum);
