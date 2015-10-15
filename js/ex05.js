d476aa0a8cc9a958eb5804b173c2e7104f5eb7e2
saji-ryu <t15443rs@sfc.keio.ac.jp>
2015-10-15 10:38:41 +0900

    ex07

--- js/ex05.js
+++ js/ex05.js
@@ -4,4 +4,4 @@ while (i < 5) {
   sum = sum + 1;
   i = i + 1;
 }
-//console.log(sum);
+console.log(sum);
---
+++ js/ex07.js
@@ -0,0 +1,32 @@
+var number = 1;
+var a = 2;
+
+onload = function () {
+  primenumber();
+}
+
+function primetest(number) {
+  if (number < 2) {
+    return false;
+  }
+  while (a <= number / 2) {
+    if (number % a == 0) {
+      a = 2;
+      return false;
+    }else {
+    a = a + 1;
+    }
+  }
+    a = 2;
+    return true;
+}
+
+function primenumber() {
+  while (number <= 100) {
+    if (primetest(number)) {
+      //pn.push(number);
+      console.log(number);
+    }
+    number = number + 1;
+  }
+}
